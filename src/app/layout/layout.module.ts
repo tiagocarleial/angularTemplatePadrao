eb6974fb7cb7000e0389754e8a1b2083f6668e5b
Tiago Carleial <tiagocarleial@gmail.com>
2022-06-21 18:57:26 -0300

    alteracoes layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 
@@ -14,13 +14,13 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     FooterComponent,
-    HeaderComponent,
-    SidebarComponent
+    HeaderComponent
+    
   ],
   declarations: [
     FooterComponent,
-    HeaderComponent,
-    SidebarComponent
+    HeaderComponent
+    
   ]
 })
 export class LayoutModule { }
